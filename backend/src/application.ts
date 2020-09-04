d63ef89460a1ecd796cb5cc6532693d444f3ad3a
tracht <marija.trachtenberg@gmail.com>
2020-09-04 12:15:18 +0100

    Adds http-error handler to application.ts

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,8 +1,30 @@
+var createError = require("http-errors");
 import * as express from "express";
-import {pingController} from "./controllers/ping";
-
 const app = express();
 
+import {pingController} from "./controllers/ping";
+import {testController} from "./controllers/test";
+
+// CONTROLLERS
 app.use(pingController);
+app.use(testController);
+
+
+// CATCH 404 AND FORWARD TO ERROR HANDLER
+app.use(function (_req, _res, next: any) {
+  next(createError(404));
+});
+
+// ERROR HANDLER
+app.use(function (err: any, req: any, res: any, _next: any) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 
 export default app;
